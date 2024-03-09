3796a1bbbd0dea31a5d859dad298390d5103bc97
gboigwe <geakande@gmail.com>
2024-03-09 14:16:20 +0100

    Reconstruct method to_dict to have a copy of the dictionary instead of creating new

--- models/base_model.py
+++ models/base_model.py
@@ -55,30 +55,8 @@ class BaseModel:
         """
         Returns a dictionary representation of all variable action
         """
-        s_dict = self.__dict__
+        s_dict = self.__dict__.copy()
         s_dict['__class__'] = self.__class__.__name__
         s_dict['created_at'] = s_dict['created_at'].isoformat()
         s_dict['updated_at'] = s_dict['updated_at'].isoformat()
         return s_dict
-
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
-print("--")
-my_new_model = BaseModel(**my_model_json)
-print(my_new_model.id)
-print(my_new_model)
-print(type(my_new_model.created_at))
-
-print("--")
-print(my_model is my_new_model)
