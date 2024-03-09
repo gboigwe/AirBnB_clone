f36fcec0d3504879b3363faacb9c1beb50193344
gboigwe <geakande@gmail.com>
2024-03-09 14:09:51 +0100

    Handled serialization to json and deserialization from json into and from a json file

--- models/base_model.py
+++ models/base_model.py
@@ -30,10 +30,7 @@ class BaseModel:
                     continue
                 elif key == 'created_at' or key == 'updated_at':
                     c = "%Y-%m-%dT%H:%M:%S.%f"
-                    try:
-                        setattr(self, key, datetime.strptime(value, c))
-                    except ValueError:
-                        print("Error: Invalid date format for {}: {}".format(key, value))
+                    setattr(self, key, datetime.strptime(value, c))
                 else:
                     setattr(self, key, value)
         else:
@@ -63,3 +60,25 @@ class BaseModel:
         s_dict['created_at'] = s_dict['created_at'].isoformat()
         s_dict['updated_at'] = s_dict['updated_at'].isoformat()
         return s_dict
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
