de366e7b9d90b294228c3f9f5bac39041911c53a
gboigwe <geakande@gmail.com>
2024-03-11 16:29:55 +0100

    Updated the id representation to a string

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,7 @@ class BaseModel:
                 else:
                     setattr(self, key, value)
         else:
-            self.id = uuid.uuid4()
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
