a366583b7a8b280c93c9eedbb66e85f268102728
gboigwe <geakande@gmail.com>
2024-03-13 11:06:05 +0100

    Corrected a console error

--- console.py
+++ console.py
@@ -19,7 +19,7 @@ from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
-from models.city import city
+from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
@@ -28,6 +28,37 @@ from models.review import Review
 class HBNBCommand(cmd.Cmd):
     """Impliment a command line action"""
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
+
+    def default(self, arg):
+        """Handling a default behavior for when cmd module
+        input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
     def do_create(self, arg):
         """
